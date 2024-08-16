9b8174322781220ef2315bb64a5dbc03e6ac0477
petermgoh <45682969+petermgoh@users.noreply.github.com>
2024-08-16 11:59:07 -0500

    password validator implemented
    added title, input, validator function, message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { useState } from 'react';
 
 function App() {
+  const validator = require('validator')
+  const [message, setMessage] = useState("")
+  const validate = (value) => {
+    if (validator.isStrongPassword(value, {minLength: 8, minLowercase: 1,
+      minUppercase: 1, minNumbers: 1, minSymbols: 1
+    })) {
+      setMessage('Is Strong Password')
+    } else {
+      setMessage('Is Not Strong Password')
+    }
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Checking Password Strength in ReactJS</h2>
+      Enter Password: 
+      <input className="input"
+      type='text'
+      placeholder='Enter Password'
+      onChange={(e) =>
+        validate(e.target.value)
+      }/><br />
+      {!message ? null : 
+      <span style={{fontWeight: 'bold', color: 'red'}}>{message}</span>}
+      <br />
+      <text>
+        Minimum Requirements: length: 8, lowercase: 1,
+        uppercase: 1, numbers: 1, symbols: 1
+      </text>
     </div>
   );
 }
